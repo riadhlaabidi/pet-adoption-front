7429fa52287133d52eb3075fb90fead6460ce5d5
riadhlaabidi <riadh.by.laabidi@gmail.com>
2023-02-18 19:33:34 +0100

    Fix popver in pets search page

--- src/app/views/search-pets/search-pets.component.ts
+++ src/app/views/search-pets/search-pets.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-pets',
   templateUrl: './search-pets.component.html',
-  styleUrls: ['./search-pets.component.css']
+  styleUrls: ['./search-pets.component.css'],
 })
-export class SearchPetsComponent {
+export class SearchPetsComponent implements OnInit {
+  sortMenuPopover = false;
 
+  ngOnInit(): void {}
+  toggleSortMenu(event: any) {
+    event.preventDefault();
+    this.sortMenuPopover = !this.sortMenuPopover;
+  }
 }
