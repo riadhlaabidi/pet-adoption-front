61ededc5dadd794293271fc1996c2e74f450d5c2
riadhlaabidi <riadh.by.laabidi@gmail.com>
2023-02-18 22:36:06 +0100

    Add signup & recover pass  page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/auth/login/login.component';
+import { RecoverPasswordComponent } from './views/auth/recover-password/recover-password.component';
+import { SignupComponent } from './views/auth/signup/signup.component';
 import { BreedsComponent } from './views/breeds/breeds.component';
 import { HomeComponent } from './views/home/home.component';
 import { SearchPetsComponent } from './views/search-pets/search-pets.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: SignupComponent },
+  { path: 'recover-password', component: RecoverPasswordComponent },
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchPetsComponent },
   { path: 'breeds', component: BreedsComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,26 @@ import { StatsComponent } from './components/stats/stats.component';
 import { LoginComponent } from './views/auth/login/login.component';
 import { SearchPetsComponent } from './views/search-pets/search-pets.component';
 import { BreedsComponent } from './views/breeds/breeds.component';
+import { RecoverPasswordComponent } from './views/auth/recover-password/recover-password.component';
+import { SignupComponent } from './views/auth/signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, BannerComponent, NotFoundComponent, NewsletterComponent, HomeComponent, FeatureComponent, CtaComponent, StatsComponent, LoginComponent, SearchPetsComponent, BreedsComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    NotFoundComponent,
+    NewsletterComponent,
+    HomeComponent,
+    FeatureComponent,
+    CtaComponent,
+    StatsComponent,
+    LoginComponent,
+    SearchPetsComponent,
+    BreedsComponent,
+    SignupComponent,
+    RecoverPasswordComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
