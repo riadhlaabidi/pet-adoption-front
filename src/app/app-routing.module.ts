102ff09098e072493c6d82beb7de286f2772e939
riadhlaabidi <riadh.by.laabidi@gmail.com>
2023-02-19 15:25:52 +0100

    Fix anonymous header nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,32 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from './views/auth/login/login.component';
-import {RecoverPasswordComponent} from './views/auth/recover-password/recover-password.component';
-import {SignupComponent} from './views/auth/signup/signup.component';
-import {BreedsComponent} from './views/breeds/breeds.component';
-import {HomeComponent} from './views/home/home.component';
-import {SearchPetsComponent} from './views/search-pets/search-pets.component';
-import {NotFoundComponent} from "./views/not-found/not-found.component";
-import {AuthGuard} from "./security/auth.guard";
-import {LoginGuard} from "./security/login.guard";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './views/auth/login/login.component';
+import { RecoverPasswordComponent } from './views/auth/recover-password/recover-password.component';
+import { SignupComponent } from './views/auth/signup/signup.component';
+import { BreedsComponent } from './views/breeds/breeds.component';
+import { HomeComponent } from './views/home/home.component';
+import { SearchPetsComponent } from './views/search-pets/search-pets.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
+import { AuthGuard } from './security/auth.guard';
+import { LoginGuard } from './security/login.guard';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent, canActivate: [LoginGuard]},
-  {path: 'register', component: SignupComponent, canActivate: [LoginGuard]},
-  {path: 'recover-password', component: RecoverPasswordComponent, canActivate: [LoginGuard]},
-  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  {path: 'search', component: SearchPetsComponent},
-  {path: 'breeds', component: BreedsComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: "**", component: NotFoundComponent}]
-
+  { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
+  { path: 'register', component: SignupComponent, canActivate: [LoginGuard] },
+  {
+    path: 'recover-password',
+    component: RecoverPasswordComponent,
+    canActivate: [LoginGuard],
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchPetsComponent },
+  { path: 'breeds', component: BreedsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
